b50ea1caa3e24aacf055b68f28cc544c4e5bad07
Franco Franzini <francofranzini12@gmail.com>
2026-05-11 17:24:34 -0300

    hecho el a)

--- tp1.hs
+++ tp1.hs
@@ -1,14 +1,9 @@
-data NdTree p = 
-  Node (NdTree p) -- sub´arbol izquierdo
-  p -- punto
-  (NdTree p) -- sub´arbol derecho
-  Int -- eje
-  | Empty
-deriving (Eq, Ord, Show)
-
 class Punto p 
   where
-    dimension :: p → Int -- devuelve el n´umero de coordenadas de un punto
-    coord :: Int → p → Double -- devuelve la coordenada k-´esima de un punto (comenzando de 0)
-    dist :: p → p → Double -- calcula la distancia entre dos puntos
+    dimension :: p -> Int -- devuelve el n´umero de coordenadas de un punto
+    coord :: Int -> p -> Double -- devuelve la coordenada k-´esima de un punto (comenzando de 0)
+    dist :: p -> p -> Double -- calcula la distancia entre dos puntos
+    dist p1 p2 = sqrt $ sum [((coord i p1) - (coord i p2)) | i <- [0..(dimension p1) - 1]]
+
+
 
