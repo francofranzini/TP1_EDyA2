f0a9c63e7a60bbc8d40a6b879fdcd5d9438e2fd5
Franco Franzini <francofranzini12@gmail.com>
2026-05-12 11:04:11 -0300

    shalom

--- tp1.hs
+++ tp1.hs
@@ -3,7 +3,8 @@ class Punto p
     dimension :: p -> Int -- devuelve el n´umero de coordenadas de un punto
     coord :: Int -> p -> Double -- devuelve la coordenada k-´esima de un punto (comenzando de 0)
     dist :: p -> p -> Double -- calcula la distancia entre dos puntos
-    dist p1 p2 = sqrt $ sum [((coord i p1) - (coord i p2)) | i <- [0..(dimension p1) - 1]]
+    dist p1 p2 = sqrt (sum [((coord i p1) - (coord i p2)) | i <- [0..(dimension p1) - 1]])
+    
 
 
 
