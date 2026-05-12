779ccf235cf2c0c90bef75728b049f933a6a0c06
Juan Ignacio Gorgo <gorgojuanignacio@gmail.com>
2026-05-12 14:41:11 -0300

    EJ4 tp1.hs

--- tp1.hs
+++ tp1.hs
@@ -77,3 +77,52 @@ insertar' x Empty k      = Node Empty x Empty k
 insertar' x (Node izq p der k) _ 
   | coord k x < coord k p = Node (insertar' x izq ((k+1) `mod` dimension p)) p der k
   | otherwise              = Node izq p (insertar' x der ((k+1) `mod` dimension p)) k
+
+---------------------------- EJERCICIO 4 ---------------------------------
+
+
+buscarKMenor :: Punto p => Int -> NdTree p -> Maybe p
+buscarKMenor _ Empty = Nothing
+buscarKMenor k (Node izq x der i) = if k == i then buscarKMenor k izq else aux minIzq x minDer
+    where
+        minIzq = buscarKMenor k izq
+        minDer = buscarKMenor k der
+        aux Nothing x Nothing   = Just x
+        aux Nothing x (Just y)  = Just (minCoord k x y)
+        aux (Just x) y Nothing  = Just (minCoord k x y)
+        aux (Just x) y (Just z) = Just (minCoord k x (minCoord k y z))
+        minCoord k x y = if (coord k x) < (coord k y) then x else y
+
+buscarKMayor :: Punto p => Int -> NdTree p -> Maybe p
+buscarKMayor _ Empty = Nothing
+buscarKMayor k (Node izq x der i) = if k == i then buscarKMayor k der else aux maxIzq x maxDer
+    where
+        maxIzq = buscarKMayor k izq
+        maxDer = buscarKMayor k der
+        aux Nothing x Nothing   = Just x
+        aux Nothing x (Just x)  = Just (maxCoord k x y)
+        aux (Just x) y Nothing  = Just (maxCoord k x y)
+        aux (Just x) y (Just z) = Just (maxcoord k x (maxcoord k z))
+        maxCoord k x y = if (coord k x) > (coord k y) then x else y
+
+compP :: Punto p => p -> p -> Int -> Bool
+compP x y k = (coord k x) < (coord k y)
+
+eliminar :: Punto p => p -> NdTree p -> NdTree p
+eliminar p Empty = Empty
+eliminar p t@(Node Empty x Empty k) = if p == x then Empty else t
+
+eliminar p t@(Node Empty x der   k) | p == x      = (Node Empty val (eliminar val der) k) 
+                                        where Just val = buscarKMenor k der
+                                    | compP p x k = t
+                                    | otherwise   = (Node Empty x (eliminar p der) k)
+
+eliminar p t@(Node izq x Empty   k) | p == x      = (Node (eliminar val izq) val Empty k)
+                                        where Just val = buscarKMayor k izq
+                                    | compP p x k = (Node (eliminar p izq) x Empty k)
+                                    | otherwise   = t
+
+eliminar p t@(Node izq x der k)     | p == x      = (Node izq val (eliminar val der) k)
+                                        where Just val = buscarKMenor k der
+                                    | compP p x k = (Node (eliminar p izq) x der k) 
+                                    | otherwise   = (Node izq x (eliminar p der) k)
